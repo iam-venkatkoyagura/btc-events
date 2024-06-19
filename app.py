12da1320649be7c2767f784a3a3789d9257ef334
venkat koyagura <venkat.koyagura@aol.com>
2024-06-19 23:17:21 +0530

    adding title and description

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def btc_events():
-    return "Welcome to BTC Events"
+    return render_template('home.html')
