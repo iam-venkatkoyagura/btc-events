2c5e56834c0a82950f5e8d25f1c0ad77451a4d37
venkat koyagura <venkat.koyagura@aol.com>
2024-06-21 15:21:15 +0530

    db abd requirements updates

--- database.py
+++ database.py
@@ -2,24 +2,23 @@ import pymysql
 import os
 
 timeout = 10
-connection = pymysql.connect(
-  charset="utf8mb4",
-  connect_timeout=timeout,
-  cursorclass=pymysql.cursors.DictCursor,
-  db=os.getenv('db'),
-  password=os.getenv("PASSWORD"),
-  host=os.getenv('host'),
-  user=os.getenv('user'),
-  read_timeout=timeout,
-  port=25039,
-  write_timeout=timeout,
+conn = pymysql.connect(
+    charset="utf8mb4",
+    connect_timeout=timeout,
+    cursorclass=pymysql.cursors.DictCursor,
+    db=os.getenv('db'),
+    password=os.getenv("PASSWORD"),
+    host=os.getenv('host'),
+    user=os.getenv('user'),
+    read_timeout=timeout,
+    port=25039,
+    write_timeout=timeout
 )
 
-def get_data():
-    cursor = connection.cursor()
-    try:
-        cursor.execute("SELECT * FROM events")
-        result = cursor.fetchall()
-    finally:
-        connection.close()
+def get_data():    
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM events")
+    result = cursor.fetchall()
+    conn.close()
     return result
+    
