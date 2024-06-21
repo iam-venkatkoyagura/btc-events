9fe6ab74314974c0d629077e14628b18f70a4491
venkat koyagura <venkat.koyagura@aol.com>
2024-06-21 16:48:07 +0530

    fixing pymysql connect error

--- database.py
+++ database.py
@@ -1,24 +1,24 @@
 import pymysql
 import os
 
-timeout = 10
-conn = pymysql.connect(
-    charset="utf8mb4",
-    connect_timeout=timeout,
-    cursorclass=pymysql.cursors.DictCursor,
-    db=os.getenv('db'),
-    password=os.getenv("PASSWORD"),
-    host=os.getenv('host'),
-    user=os.getenv('user'),
-    read_timeout=timeout,
-    port=25039,
-    write_timeout=timeout
-)
-
-def get_data():    
+def get_data():   
+    timeout = 10
+    conn = pymysql.connect(
+        charset="utf8mb4",
+        connect_timeout=timeout,
+        cursorclass=pymysql.cursors.DictCursor,
+        db=os.getenv('db'),
+        password=os.getenv("PASSWORD"),
+        host=os.getenv('host'),
+        user=os.getenv('user'),
+        read_timeout=timeout,
+        port=25039,
+        write_timeout=timeout) 
+        
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM events")
     result = cursor.fetchall()
     conn.close()
     return result
+    
     
